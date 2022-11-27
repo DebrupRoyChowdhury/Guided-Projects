7320948cfe75e743bfb2619c8456749d6c4ac060
DebrupRoyChowdhury <debruproychowdhury@gmail.com>
2022-11-27 19:21:33 +0530

    3. Rock, Paper, Scissors Updated

--- Kylie Ying/rock_paper_scissor.py
+++ Kylie Ying/rock_paper_scissor.py
@@ -1,14 +1,13 @@
-from random import *
-
+from random import randint
 
 class game:
     def __init__(self):
-        self.player_name = input('Enter Name Please: ')
-        self.__new_game()
+        self.__player_name = input('Enter Name Please: ')
+        self.__init_new_game()
 
 
-    def __return_player_name(self):
-        return self.player_name
+    def return_player_name(self):
+        return self.__player_name
 
 
     @staticmethod
@@ -17,91 +16,114 @@ class game:
             return 'Rock'
         elif num_choice == 1:
             return 'Paper'
-        else:
+        elif num_choice == 2:
             return 'Scissors'
 
 
-    def __new_game(self):
-        no_of_rounds = 0
-
+    def __init_new_game(self):
         print('\nWelcome To The Ultimate Rock, Paper, Scissors Game\n')
 
-        print(f'This Match Is Between {self.__return_player_name} & La Máquina')
+        print(f'This Match Is Between {self.__player_name} & La Máquina')
         print('------------------------------------------------------------------------------------------------------------')
-        
         print('Rules:')
         print('\t1. Enter 0 For Rock, 1 For Paper, 2 For Scissor.')
         print('\t2. Take Fun Seriously. Entering Wrong Input Twice In A Single Game Will Result In An Instant Lose.')
         print("\t3. Relax Dude. It's Just A Game!")
-        
         print('------------------------------------------------------------------------------------------------------------')
         
-        print('\nNow! How Many Rounds Do You Want To Challenge The Machine For?\n')
+        print('\nNow! How Many Rounds Do You Want To Challenge La Máquina For?\n')
 
         while(1):
             try:
-                no_of_rounds = int(input('Enter Here: '))
+                self.no_of_rounds = int(input('Enter Here: '))
             except ValueError:
                 print('\nPlease Enter Valid Number Of Rounds\n')
             else:
                 break
-
         print('------------------------------------------------------------------------------------------------------------')
 
-        self.__in_match(no_of_rounds)
+        self.__match()
 
 
-    def __in_match(self, no_of_rounds):
-        no_of_rounds, prank_count, player_wins, computer_wins = 0, 0, 0
-        playr_name = self.__return_player_name()
+    def __match(self):
+        self.__prank_count, self.__player_wins, self.__computer_wins = 0, 0, 0
 
-        for round in range(1, no_of_rounds + 1):
+        for round in range(1, self.no_of_rounds + 1):
             computer_choice = randint(0, 2)
+            value = self.__round(computer_choice, round)
 
-            print(f'\nRound {round}')
-            print(f"------{'-' * round}-")
-            print(f'\nLa Máquina Has Decided. Now You Decide {playr_name}!')
-
-            while 1:
-                try:
-                    player_choice = int(input('Enter Your Choice: '))
-                except ValueError:
-                    prank_count += 1
-                    print(f'You Now Have Only {2 - prank_count} Chances Left To Give Correct Input')
-                else:
-                    if player_choice < 0 or player_choice > 2:
-                        prank_count += 1
-                        print(f'You Now Have Only {2 - prank_count} Chances Left To Give Correct Input')
-                        continue
-                    if prank_count == 2:
-                        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!YOU LOST DUDE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-                        return
-                    break
-
-            print(f'\n{playr_name} chose {self.__choice(player_choice)} & La Máquina chose {self.__choice(computer_choice)}\n')
-
-            if (player_choice + 1) % 3 == computer_choice:
-                print('La Máquina Wins')
-                computer_wins = computer_wins + 1
-            elif (computer_choice + 1) % 3 == player_choice:
-                print(f'{playr_name} Wins')
-                player_wins = player_wins + 1
-            elif computer_choice == player_choice:
-                print('\nIt Is A Tie. No Points Awarded.\n')
-
-            if player_wins > (no_of_rounds / 2):
-                print(f'\n{playr_name} Has Won Due To Technical Victory!!!!!!!! Hurray!!!!!!!!!!\n')
+            if value == 1:
+                print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!YOU LOST DUDE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
                 return
-            elif computer_wins > (no_of_rounds / 2):
+            if value == 2:
+                print(f'\n{self.__player_name} Has Won Due To Technical Victory!!!!!!!! Hurray!!!!!!!!!!\n')
+                return
+            if value == 3:
                 print(f'\nLa Máquina Has Won Due To Technical Victory!!!!!!!! Hurray!!!!!!!!!!\n')
                 return
-
-            print(f'\nCurrent Tally: La Máquina {computer_wins}, {playr_name} {player_wins}.\n')
-            print('------------------------------------------------------------------------------------------------------------')
-
-        if computer_wins > player_wins:
+    
+        if self.__computer_wins > self.__player_wins:
             print('\nLa Máquina Has Won!!!!\n')
-        elif player_wins > computer_wins:
-            print(f'\n{playr_name} Has Won!!!!\n')
+        elif self.__player_wins > self.__computer_wins:
+            print(f'\n{self.__player_name} Has Won!!!!\n')
         else:
-            print('\nIt Is A Tie! Congratulations!! No One Wins!!')
+            print('\nIt Is A Tie! Congratulations!! No One Wins!!')    
+
+
+    def __round(self, computer_choice, round):
+        playr_name = self.return_player_name()
+
+        print(f'\nRound {round}')
+        print(f"------{'-' * round}--")
+        print(f'\nLa Máquina Has Decided. Now You Decide {playr_name}!')
+
+        player_choice = self.__choose_player()
+
+        if player_choice == 3:
+            return 1
+        
+        print(f'\n{playr_name} chose {game.__choice(player_choice)} & La Máquina chose {game.__choice(computer_choice)}\n')
+
+        self.__decision(player_choice, computer_choice)
+
+        if self.__player_wins > (self.no_of_rounds / 2):
+            return 2
+        elif self.__computer_wins > (self.no_of_rounds / 2):
+            return 3
+
+        print(f'\nCurrent Tally: La Máquina {self.__computer_wins}, {playr_name} {self.__player_wins}.\n')
+        print('------------------------------------------------------------------------------------------------------------')
+        
+        return 0
+
+
+    def __choose_player(self):
+        while 1:
+            if self.__prank_count == 2:
+                return 3
+
+            try:
+                player_choice = int(input('Enter Your Choice: '))            
+            except ValueError:
+                self.__prank_count += 1
+                print(f'You Now Have Only {2 - self.__prank_count} Chances Left To Give Correct Input')
+            else:
+                if player_choice < 0 or player_choice > 2:
+                    self.__prank_count += 1
+                    print(f'You Now Have Only {2 - self.__prank_count} Chances Left To Give Correct Input')
+                    continue
+                
+                return player_choice
+
+
+    def __decision(self, player_choice, computer_choice):
+        if (player_choice + 1) % 3 == computer_choice:
+            print('La Máquina Wins')
+            self.__computer_wins += 1
+
+        elif (computer_choice + 1) % 3 == player_choice:
+            print(f'{self.__player_name} Wins')
+            self.__player_wins += 1
+
+        elif computer_choice == player_choice:
+            print('\nIt Is A Tie. No Points Awarded.\n')
